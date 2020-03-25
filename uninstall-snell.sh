86283b715608b0975fd2e532431d10ed05bea8d2
ascfin <ascfin@gmail.com>
2020-03-25 15:40:16 +0800

    Update uninstall-snell.sh

--- uninstall-snell.sh
+++ uninstall-snell.sh
@@ -3,7 +3,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 systemctl stop snell
 systemctl disable snell
-rm -f /etc/systemd/snell.service
+rm -f /etc/systemd/system/snell.service
 rm -f /usr/bin/snell-server
-rm -f /etc/snell/snell-server.conf
 rm -rf /etc/snell/
